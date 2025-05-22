dd95b698c890531a161f4ac4c0559b0365120694
pascaladnan101@gmail.com <pascaladnan101@gmail.com>
2025-05-22 15:02:08 +0700

    refactor: simplify master layout by removing unnecessary row wrapper around content yield

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -17,9 +17,7 @@
             @include('partials.header')
             <div class="page-body">
                 <div class="container-xl">
-                    <div class="row row-deck row-cards">
-                        @yield('content')
-                    </div>
+                    @yield('content')
                 </div>
             </div>
         </div>
