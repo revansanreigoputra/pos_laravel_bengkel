fc72d14a35bcaf01d489ea07d70b4ef3ba2d1f8e
pascaladnan101@gmail.com <pascaladnan101@gmail.com>
2025-05-22 14:41:47 +0700

    refactor: remove placeholder card elements and integrate content yield in master layout

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,75 +18,7 @@
             <div class="page-body">
                 <div class="container-xl">
                     <div class="row row-deck row-cards">
-                        <div class="col-sm-6 col-lg-3">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem"></div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 col-lg-3">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem"></div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 col-lg-3">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem"></div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6 col-lg-3">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem"></div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="row row-cards">
-                                <div class="col-12">
-                                    <div class="card">
-                                        <div class="card-body" style="height: 10rem"></div>
-                                    </div>
-                                </div>
-                                <div class="col-12">
-                                    <div class="card">
-                                        <div class="card-body" style="height: 10rem"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem"></div>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem"></div>
-                            </div>
-                        </div>
-                        <div class="col-md-12 col-lg-8">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem"></div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-4">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem"></div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-4">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem"></div>
-                            </div>
-                        </div>
-                        <div class="col-md-12 col-lg-8">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem"></div>
-                            </div>
-                        </div>
-                        <div class="col-12">
-                            <div class="card">
-                                <div class="card-body" style="height: 10rem"></div>
-                            </div>
-                        </div>
+                        @yield('content')
                     </div>
                 </div>
             </div>
