3486496ecfed551b60d1bd191712526df29df300
revansanreigoputra <revanasanreigo1@gmail.com>
2025-07-16 21:08:49 +0700

    Selesaikan konflik PermissionSeeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -15,13 +15,11 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
         $permissions = [
-<<<<<<< Updated upstream
-=======
+
             'service.view',
             'service.create',
             'service.update',
             'service.delete',
->>>>>>> Stashed changes
             'role.view',
             'role.update',
             'category.view',
